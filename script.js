0f7e99ce32e7dd575b227ea71d74ada083778f78
Erjon Rama <erjonrama05@gmail.com>
2025-02-02 02:56:44 +0100

    script.js

--- script.js
+++ script.js
@@ -23,3 +23,52 @@ iconClose.addEventListener('click', (e) => {
     e.preventDefault();
     wrapper.classList.remove('active-popup');
 });
+
+
+let currentSlide = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+});
+function openMap(mapUrl) {
+    let mapContainer = document.getElementById('mapContainer');
+
+    if (!mapContainer) {
+
+        mapContainer = document.createElement('div');
+        mapContainer.id = 'mapContainer';
+
+        const mapFrame = document.createElement('iframe');
+
+        const closeButton = document.createElement('button');
+        closeButton.id = 'closeButton';
+        closeButton.textContent = 'x';
+        closeButton.onclick = closeMap;
+        mapContainer.appendChild(mapFrame);
+        mapContainer.appendChild(closeButton);
+        document.body.appendChild(mapContainer);
+    }
+    const mapFrame = document.getElementById('mapFrame');
+    mapFrame.src = mapUrl;
+    mapContainer.style.display = 'block';
+}
+function closeMap() {
+    const mapContainer = document.getElementById('mapContainer');
+
+    if (mapContainer) {
+        mapContainer.style.display = 'none';
+        const mapFrame = document.getElementById('mapFrame');
+        if (mapFrame) {
+            mapFrame.src = '';
+        }
+    }
+}
